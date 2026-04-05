14e90caf0ce29329299e6151f5973f7e2ae2a7d7
liton-das <litonchandra094@gmail.com>
2026-04-05 18:59:16 +0300

    updated in backend index file

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:'https://fullstack-project-green.vercel.app',
+    origin:'https://fullstack-project-green.vercel.app/',
     credentials:true,
 }))
 app.use(route)
