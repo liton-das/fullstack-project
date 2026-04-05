1fd56515a45a2396ffaf1f13146922b7317c398b
liton-das <litonchandra094@gmail.com>
2026-04-05 18:22:04 +0300

    cors url change from backend index file

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://fullstack-project-green.vercel.app/',
     credentials:true,
 }))
 app.use(route)
