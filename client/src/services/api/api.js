e736a6fa6fcbcd118ca59b20ddeda02c6595b9f9
liton-das <litonchandra094@gmail.com>
2026-04-05 21:45:11 +0300

    change url in client from api.js

--- client/src/services/api/api.js
+++ client/src/services/api/api.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const baseQuery = fetchBaseQuery({ baseUrl: `https://fullstack-project-nsri.vercel.app`, credentials: "include" });
+const baseQuery = fetchBaseQuery({ baseUrl: `https://fullstack-project-backend-one.vercel.app`, credentials: "include" });
 const ReAuth = async (arg, api, extraOptions) => {
   let result = await baseQuery(arg, api, extraOptions);
   if (result.error?.status === 401) {
