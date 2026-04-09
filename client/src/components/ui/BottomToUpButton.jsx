816ee97a007c1a5dfdb95de8fea819dc23dd495b
liton-das <litonchandra094@gmail.com>
2026-04-09 18:28:43 +0300

    update the scroll button

--- client/src/components/ui/BottomToUpButton.jsx
+++ client/src/components/ui/BottomToUpButton.jsx
@@ -25,11 +25,11 @@ const BottomToUpButton = () => {
   return (
     <>
       {/* Botton to up animated button */}
-      <div className="fixed bottom-4 right-4">
+      <div className="fixed bottom-18 right-4">
         {showButton && (
           <button
             onClick={scrollToTop}
-            className="cursor-pointer animate-bounce p-3 rounded-full bg-blue-600 text-white shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-opacity duration-300 opacity-80 hover:opacity-100"
+            className="cursor-pointer animate-bounce p-3 rounded-full bg-blue-600 text-white shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-opacity duration-300 opacity-80 hover:opacity-100 z-50"
           >
             <FaArrowUp />
           </button>
